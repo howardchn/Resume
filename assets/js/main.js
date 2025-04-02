6253cd26a452177e4fcd2634613dd6095e01a8e6
howardch <howardch@outlook.com>
2025-04-02 21:50:12 +0800

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,7 +14,7 @@ var appData = {
                     url: 'https://empathia.ai',
                     summary: 'Technical Project Director',
                     date: '2023/10 - Now',
-                    work: "Empathia is a startup company that focuses on AI scribing for healthcare assistant. Empathia and the previous company were created by the same owner and were formed by several key members from the previous company who moved over. As a director, responsible for the design, development, coordination, and testing release of multiple key products in the product family, as well as subsequent iterations. Consider comprehensively the interaction with other modules and the future development direction in the product lines I'm responsible for.",
+                    work: "Empathia is a startup company that focuses on AI scribing for healthcare assistant. Empathia and the previous company were created by the same owner and were formed by several key members from the previous company who moved over. Designed and developed the prototype of the company's current product for fundraising. Later, responsible for the design, development, coordination, testing, and release of multiple major products in the product family, as well as subsequent iterations. Additionally, for several product lines under my responsibility, I comprehensively considered the interaction with other modules and the future development direction.",
                 },
                 {
                     name: 'MobLab',
